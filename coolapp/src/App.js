43a7bcd73229c304b3d0e0d92c36783b1f1ef990
FootLettuce26 <caleb.weigt@cuw.edu>
2023-10-25 14:23:45 -0500

    test

--- coolapp/src/App.js
+++ coolapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code> This is the correct file!</code>
+          <code> This is the correct file! I am able to push it!</code>
         </p>
         <a
           className="App-link"
