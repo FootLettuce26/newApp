8b8fb1f9c16c2eecb9d178a472c9aabd39b12c36
FootLettuce26 <caleb.weigt@cuw.edu>
2023-11-02 13:54:42 -0500

    update as of 11/2

--- coolapp/src/App.js
+++ coolapp/src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import Login from './Pages/Login';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
+    <Router>
+      <div>
+        <Link to="/Login"> 
+          <button>Login</button>
+        </Link>
+
+        
+        <Routes>
+          <Route path="/Login" component={Login} />
+        </Routes>
+        
+      </div>
+    </Router>
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code> This is the correct file! I am able to push it!</code>
+          <code> This is the correct file!</code>
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
-    </div>
+      <div>
+        
+      </div>
+    </div>*/
   );
 }
 
-export default App;
